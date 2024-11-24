8f7ca48589148d3c8703480cdd7df15f81852a75
fsaracho <sarachofacundot@gmail.com>
2024-11-24 15:29:23 -0300

    Improved RestExceptionHandler

--- src/main/java/com/facundosaracho/weatherapi/exception/ErrorDetails.java
+++ src/main/java/com/facundosaracho/weatherapi/exception/ErrorDetails.java
@@ -8,6 +8,7 @@ public enum ErrorDetails {
     //Server exceptions
     GENERIC_EXCEPTION(5000, "GENERIC ERROR"),
     RETROFIT_ERROR(5001, "Could not validate retrofit call."),
+    NO_RESOURCE_FOUND(5002, "Invalid path. Resource was not found."),
 
     //BusinessExceptions
     WEATHER_NOT_FOUND(4000, "Weather was not found");
--- src/main/java/com/facundosaracho/weatherapi/presentation/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/facundosaracho/weatherapi/presentation/exception/handler/RestExceptionHandler.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import static com.facundosaracho.weatherapi.exception.ErrorDetails.GENERIC_EXCEPTION;
+import static com.facundosaracho.weatherapi.exception.ErrorDetails.NO_RESOURCE_FOUND;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -22,12 +24,21 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(formGenericErrorDto(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    ResponseEntity<ErrorResponseDto> handleInvalidResourceException() {
+        return new ResponseEntity<>(formNoResourceFoundErrorDto(), HttpStatus.NOT_FOUND);
+    }
+
     private ErrorResponseDto formErrorDto(RestException restException) {
         return new ErrorResponseDto(restException.getErrorDetails().getCode(), restException.getErrorDetails().getMessage());
     }
 
     private ErrorResponseDto formGenericErrorDto() {
         return new ErrorResponseDto(GENERIC_EXCEPTION.getCode(), GENERIC_EXCEPTION.getMessage());
+    }
+
+    private ErrorResponseDto formNoResourceFoundErrorDto() {
+        return new ErrorResponseDto(NO_RESOURCE_FOUND.getCode(), NO_RESOURCE_FOUND.getMessage());
     }
 }
 
