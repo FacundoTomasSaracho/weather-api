afba27dadaf2981b6eceefae5c984e56776a4047
fsaracho <sarachofacundot@gmail.com>
2024-11-24 15:24:38 -0300

    Fixed lombok+mapstruct getter and setter annotation problem

--- src/main/java/com/facundosaracho/weatherapi/business/model/client/weatherpapi/WeatherApiResponse.java
+++ src/main/java/com/facundosaracho/weatherapi/business/model/client/weatherpapi/WeatherApiResponse.java
@@ -1,11 +1,11 @@
 package com.facundosaracho.weatherapi.business.model.client.weatherpapi;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@Getter
+@Setter
 public class WeatherApiResponse {
 
     @JsonProperty("location")
@@ -14,88 +14,9 @@ public class WeatherApiResponse {
     @JsonProperty("current")
     private Current current;
 
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public Current getCurrent() {
-        return current;
-    }
-
-    public void setCurrent(Current current) {
-        this.current = current;
-    }
-
+    @Getter
+    @Setter
     public static class Location {
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getRegion() {
-            return region;
-        }
-
-        public void setRegion(String region) {
-            this.region = region;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
-
-        public double getLat() {
-            return lat;
-        }
-
-        public void setLat(double lat) {
-            this.lat = lat;
-        }
-
-        public double getLon() {
-            return lon;
-        }
-
-        public void setLon(double lon) {
-            this.lon = lon;
-        }
-
-        public String getTzId() {
-            return tzId;
-        }
-
-        public void setTzId(String tzId) {
-            this.tzId = tzId;
-        }
-
-        public long getLocaltimeEpoch() {
-            return localtimeEpoch;
-        }
-
-        public void setLocaltimeEpoch(long localtimeEpoch) {
-            this.localtimeEpoch = localtimeEpoch;
-        }
-
-        public String getLocaltime() {
-            return localtime;
-        }
-
-        public void setLocaltime(String localtime) {
-            this.localtime = localtime;
-        }
 
         @JsonProperty("name")
         private String name;
@@ -123,239 +44,9 @@ public class WeatherApiResponse {
     }
 
 
+    @Getter
+    @Setter
     public static class Current {
-
-        public long getLastUpdatedEpoch() {
-            return lastUpdatedEpoch;
-        }
-
-        public void setLastUpdatedEpoch(long lastUpdatedEpoch) {
-            this.lastUpdatedEpoch = lastUpdatedEpoch;
-        }
-
-        public String getLastUpdated() {
-            return lastUpdated;
-        }
-
-        public void setLastUpdated(String lastUpdated) {
-            this.lastUpdated = lastUpdated;
-        }
-
-        public double getTempC() {
-            return tempC;
-        }
-
-        public void setTempC(double tempC) {
-            this.tempC = tempC;
-        }
-
-        public double getTempF() {
-            return tempF;
-        }
-
-        public void setTempF(double tempF) {
-            this.tempF = tempF;
-        }
-
-        public int getIsDay() {
-            return isDay;
-        }
-
-        public void setIsDay(int isDay) {
-            this.isDay = isDay;
-        }
-
-        public Condition getCondition() {
-            return condition;
-        }
-
-        public void setCondition(Condition condition) {
-            this.condition = condition;
-        }
-
-        public double getWindMph() {
-            return windMph;
-        }
-
-        public void setWindMph(double windMph) {
-            this.windMph = windMph;
-        }
-
-        public double getWindKph() {
-            return windKph;
-        }
-
-        public void setWindKph(double windKph) {
-            this.windKph = windKph;
-        }
-
-        public int getWindDegree() {
-            return windDegree;
-        }
-
-        public void setWindDegree(int windDegree) {
-            this.windDegree = windDegree;
-        }
-
-        public String getWindDir() {
-            return windDir;
-        }
-
-        public void setWindDir(String windDir) {
-            this.windDir = windDir;
-        }
-
-        public double getPressureMb() {
-            return pressureMb;
-        }
-
-        public void setPressureMb(double pressureMb) {
-            this.pressureMb = pressureMb;
-        }
-
-        public double getPressureIn() {
-            return pressureIn;
-        }
-
-        public void setPressureIn(double pressureIn) {
-            this.pressureIn = pressureIn;
-        }
-
-        public double getPrecipMm() {
-            return precipMm;
-        }
-
-        public void setPrecipMm(double precipMm) {
-            this.precipMm = precipMm;
-        }
-
-        public double getPrecipIn() {
-            return precipIn;
-        }
-
-        public void setPrecipIn(double precipIn) {
-            this.precipIn = precipIn;
-        }
-
-        public int getHumidity() {
-            return humidity;
-        }
-
-        public void setHumidity(int humidity) {
-            this.humidity = humidity;
-        }
-
-        public int getCloud() {
-            return cloud;
-        }
-
-        public void setCloud(int cloud) {
-            this.cloud = cloud;
-        }
-
-        public double getFeelslikeC() {
-            return feelslikeC;
-        }
-
-        public void setFeelslikeC(double feelslikeC) {
-            this.feelslikeC = feelslikeC;
-        }
-
-        public double getFeelslikeF() {
-            return feelslikeF;
-        }
-
-        public void setFeelslikeF(double feelslikeF) {
-            this.feelslikeF = feelslikeF;
-        }
-
-        public double getWindchillC() {
-            return windchillC;
-        }
-
-        public void setWindchillC(double windchillC) {
-            this.windchillC = windchillC;
-        }
-
-        public double getWindchillF() {
-            return windchillF;
-        }
-
-        public void setWindchillF(double windchillF) {
-            this.windchillF = windchillF;
-        }
-
-        public double getHeatindexC() {
-            return heatindexC;
-        }
-
-        public void setHeatindexC(double heatindexC) {
-            this.heatindexC = heatindexC;
-        }
-
-        public double getHeatindexF() {
-            return heatindexF;
-        }
-
-        public void setHeatindexF(double heatindexF) {
-            this.heatindexF = heatindexF;
-        }
-
-        public double getDewpointC() {
-            return dewpointC;
-        }
-
-        public void setDewpointC(double dewpointC) {
-            this.dewpointC = dewpointC;
-        }
-
-        public double getDewpointF() {
-            return dewpointF;
-        }
-
-        public void setDewpointF(double dewpointF) {
-            this.dewpointF = dewpointF;
-        }
-
-        public double getVisKm() {
-            return visKm;
-        }
-
-        public void setVisKm(double visKm) {
-            this.visKm = visKm;
-        }
-
-        public double getVisMiles() {
-            return visMiles;
-        }
-
-        public void setVisMiles(double visMiles) {
-            this.visMiles = visMiles;
-        }
-
-        public double getUv() {
-            return uv;
-        }
-
-        public void setUv(double uv) {
-            this.uv = uv;
-        }
-
-        public double getGustMph() {
-            return gustMph;
-        }
-
-        public void setGustMph(double gustMph) {
-            this.gustMph = gustMph;
-        }
-
-        public double getGustKph() {
-            return gustKph;
-        }
-
-        public void setGustKph(double gustKph) {
-            this.gustKph = gustKph;
-        }
 
         @JsonProperty("last_updated_epoch")
         private long lastUpdatedEpoch;
@@ -446,32 +137,9 @@ public class WeatherApiResponse {
 
     }
 
-
+    @Getter
+    @Setter
     public static class Condition {
-
-        public String getText() {
-            return text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
-        }
-
-        public String getIcon() {
-            return icon;
-        }
-
-        public void setIcon(String icon) {
-            this.icon = icon;
-        }
-
-        public int getCode() {
-            return code;
-        }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
 
         @JsonProperty("text")
         private String text;
